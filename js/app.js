4c2d271ca94c6d0def8338a533dbe09cd6bba9a2
Maggie Gong <meijic@outlook.com>
2019-06-15 13:05:47 -0400

    Move entities!
    Player can be moved by keyboard inputs. Enemies are moving at a constant speed.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,27 @@
 // Enemies our player must avoid
 var Enemy = function(x, y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.x = x * 101;
-    this.y = y * 83 - 20;
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/enemy-bug.png';
+  this.x = x * 101;
+  this.y = y * 83 - 20;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  this.x = this.x + 40 * dt; // move the enemies at 40 pixels/second
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
@@ -37,6 +38,36 @@ Player.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+Player.prototype.update = function() {
+  // This function is useless at the moment!
+};
+
+Player.prototype.handleInput = function(key) {
+    // Move player according to keyboard input
+    // If statements are used to detect canvas boundaries
+    switch (key) {
+      case 'left':
+        if (this.x > 0) {
+          this.x = this.x - 101;
+        }
+        break;
+      case 'right':
+        if (this.x < 606) {
+          this.x = this.x + 101;
+        }
+        break;
+      case 'up':
+        if (this.y > -20) {
+          this.y = this.y - 83;
+        }
+        break;
+      case 'down':
+        if (this.y < 395) {
+          this.y = this.y + 83;
+        }
+    }
+};
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
@@ -49,12 +80,12 @@ var allEnemies = [enemy1, enemy2];
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
